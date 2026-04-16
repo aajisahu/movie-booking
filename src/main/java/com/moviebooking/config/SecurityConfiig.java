2c7eeb738a858140cd99a7036c761e047c262e26
ASHISH\aajis <aajisahu@gmail.com>
2026-04-16 22:11:06 +0530

    added securityFilterChain for enable request matcher

--- src/main/java/com/moviebooking/config/SecurityConfiig.java
+++ src/main/java/com/moviebooking/config/SecurityConfiig.java
@@ -2,6 +2,7 @@ package com.moviebooking.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,8 +17,17 @@ public class SecurityConfiig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(){
-     return null;
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csfr -> csfr.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                        );
+
+     return http.build();
     }
 
 }
